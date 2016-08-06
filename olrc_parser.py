e6fc399d536425e158d7706d00cc574fe6f5d5aa
Santhosh Balasubramanian <santhoshbala@gmail.com>
2016-08-05 23:16:57 -0700

    parser produces sanitized html with no styles

--- olrc_parser.py
+++ olrc_parser.py
@@ -6,7 +6,7 @@
 # Public Laws. They publish data in a variety of formats, but this class
 # provides all the utility necessary to parse and manipulator that data.
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 
 # Class for parsing OLRC data, based on BeautifulSoup.
 class OlrcParser:
@@ -23,13 +23,12 @@ class OlrcParser:
 		self.format = format
 		self.inFile = open(inFileName)
 
-
 		return
 
 	# Parse the file provided as input and write the output to outfile.
 	def parse(self, outFileName):
 		if self.format == self.CODE_FORMAT_XHTML:
-			self._parseXML(outFileName)
+			self._sanitizeXML(outFileName)
 		elif self.format == self.CODE_FORMAT_PDF:
 			# TODO(santhoshbala): Write PDF parser.
 			pass
@@ -39,13 +38,12 @@ class OlrcParser:
 
 		return
 
-	def _parseXML(self, outFileName):
+	def _sanitizeXML(self, outFileName):
 		
 		xmlSoup = BeautifulSoup(self.inFile, OlrcParser.XML_PARSER)
-		self._deleteXmlHeaderUI(xmlSoup)
 
-		self._deleteXmlMenu(xmlSoup)
-		self._deleteXmlResizeWindow(xmlSoup)
+		self._deleteEditorialNotes(xmlSoup)
+		self._deleteInlineStyles(xmlSoup)
 
 		outFile = open(outFileName, 'w')
 		outFile.write(xmlSoup.prettify("utf-8"))
@@ -53,54 +51,13 @@ class OlrcParser:
 
 		return
 
-	# USC XML files contain header UI at the top of the document.
-	# Delete that UI for the sake of removing non-code text.
-	def _deleteXmlHeaderUI(self, xmlSoup):
-		header = xmlSoup.find("div", {"id" : "header"})
-		header.extract()
-
-		return
-
-	# USC XML files contain a menu UI at the top of the document.
-	# Delete that UI for the sake of removing non-code text.
-	def _deleteXmlMenu(self, xmlSoup):
-		menu = xmlSoup.find("div", {"id" : "menu"})
-		menu.extract()
-
-		return
-
-	# USC XML files contain a hidden resizeWindow div.
-	# Delete that div for the sake of clarity.
-	def _deleteXmlResizeWindow(self, xmlSoup):
-		resizeWindowDiv = xmlSoup.find("div", {"id" : "resizeWindow"})
-		try:
-			resizeWindowDiv.extract()
-		except AttributeError:
-			pass
-
-		resizeWindowSpan = xmlSoup.find("span", {"id" : "resizeWindow"})
-		try:
-			resizeWindowSpan.extract()
-		except AttributeError:
-			pass
-
-		return
-
-	# Delete footer XML for sake of clarity.
-	def _deleteXmlFooter(self, xmlSoup):
-		footer = xmlSoup.find("div", {"id" : "footer"})
-		footer.extract()
-
-		return
-
-	# USC XML files contain XML comments indicating where pagebreaks
-	# would occur in the corresponding PDF version.
-	# Delete these comments to minimize unnecessary diffs, because
-	# the positions of these breaks certainly change over time.
-	def _deletePdfPageBreaks(self, xmlSoup):
-		pageBreaks = xmlSoup.findAll(text=lambda text:isinstance(text, Comment) and " PDFPage" in text)
-		for pageBreak in pageBreaks:
-			pageBreak.extract()
+	# Delete all inline styles to prepare for injecting new styles.
+	def _deleteInlineStyles(self, xmlSoup):
+		for tag in xmlSoup.recursiveChildGenerator():
+			try:
+				tag.attrs['style'] = None
+			except AttributeError:
+				pass
 
 		return
 
@@ -109,20 +66,65 @@ class OlrcParser:
 	# (2) notes about other sections which refer to the current section
 	# (3) cross-references to other legal documents
 	# (4) other miscellaneous notes
+	# (5) <script>, <br>, etc.
 	def _deleteEditorialNotes(self, xmlSoup):
-		# To get rid of all notes, need to delete classes matching "note-head"
-		# and "note-body", as well as the comments for "repeal-note", "secref",
-		# "sectionreferredto", "amendment-note", "crossreference-note", and
-		# "miscellaneous-note".
-		pClasses = ["note-head", "note-body"]
-		commentTypes = ["repeal-note", "secref", "sectionreferredto", "amendment-note", 
-						"crossreference-note", "miscellaneous-note"]
+		# To get rid of all notes, need to delete <table>, <div>, <p>, <h4>, and <!--> tags.
+		tableClasses = ["uscdispo3col"]
+		hClasses = ["note-head", "note-sub-head", "source-credit"]
+		pClasses = ["note-head", "note-body", "note-body-1em", "footnote", "presidential-signature"]
+		divClasses = ["analysis"]
+		divIds = ["footer", "resizeWindow", "menu", "menu_homeLink", "header"]
+		spanIds = ["resizeWindow"]
+		tagTypes = ["br", "meta", "script", "noscript", "link"]
+		commentTypes = ["notes", "repeal-note", "secref", "sectionreferredto", "amendment-note", 
+						"crossreference-note", "miscellaneous-note", "source-credit",
+						"footnote", "analysis", "effectivedate-note", "documentid:",
+						" PDFPage", "usckey:", "itemsortkey:", "itempath:", "HTTP"]
+
+		for tableClass in tableClasses:
+			tags = xmlSoup.findAll("table", {"class" : tableClass})
+			for tag in tags:
+				try:
+					tag.extract()
+				except AttributeError:
+					pass
+
+		for hClass in hClasses:
+			tags = xmlSoup.findAll("h4", {"class" : hClass})
+			for tag in tags:
+				try:
+					tag.extract()
+				except AttributeError:
+					pass
 
 		for pClass in pClasses:
 			tags = xmlSoup.findAll("p", {"class" : pClass})
 			for tag in tags:
 				try:
-					print pClass
+					tag.extract()
+				except AttributeError:
+					pass
+
+		for divClass in divClasses:
+			tags = xmlSoup.findAll("div", {"class" : divClass})
+			for tag in tags:
+				try:
+					tag.extract()
+				except AttributeError:
+					pass
+
+		for divId in divIds:
+			tags = xmlSoup.findAll("div", {"id" : divId})
+			for tag in tags:
+				try:
+					tag.extract()
+				except AttributeError:
+					pass
+
+		for spanId in spanIds:
+			tags = xmlSoup.findAll("span", {"id" : spanId})
+			for tag in tags:
+				try:
 					tag.extract()
 				except AttributeError:
 					pass
@@ -131,7 +133,14 @@ class OlrcParser:
 			tags = xmlSoup.findAll(text=lambda text:isinstance(text, Comment) and commentType in text)
 			for tag in tags:
 				try:
-					print commentType
+					tag.extract()
+				except AttributeError:
+					pass
+
+		for tagType in tagTypes:
+			tags = xmlSoup.findAll(tagType)
+			for tag in tags:
+				try:
 					tag.extract()
 				except AttributeError:
 					pass
