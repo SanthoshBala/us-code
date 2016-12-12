21fbf0252dae3fff2ebc043059a37125ead2223d
Santhosh Balasubramanian <santhoshbala@gmail.com>
2016-12-11 21:12:49 -0800

    wip state for speedtest -- confirmed lxml 10x faster

--- parser_speed_test.py
+++ parser_speed_test.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup, Comment, NavigableString
 from lxml import etree
 from string_utils import unescape
 import time
+import io
 
 entities = [
     ('&nbsp;', u'\u00a0'),
@@ -19,14 +20,51 @@ entities = [
     ('&minus;', u'\u002D')
     ]
 
+xslt = '''<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="xml" indent="no"/>
+
+<xsl:template match="/|comment()|processing-instruction()">
+    <xsl:copy>
+      <xsl:apply-templates/>
+    </xsl:copy>
+</xsl:template>
+
+<xsl:template match="*">
+    <xsl:element name="{local-name()}">
+      <xsl:apply-templates select="@*|node()"/>
+    </xsl:element>
+</xsl:template>
+
+<xsl:template match="@*">
+    <xsl:attribute name="{local-name()}">
+      <xsl:value-of select="."/>
+    </xsl:attribute>
+</xsl:template>
+</xsl:stylesheet>
+'''
+
+pClasses = set(["note-head", "note-body", "note-body-1em", "footnote", "presidential-signature",
+                    "note-body-flush0_hang1", "note-body-block", "note-body-2em", "note-body-flush2_hang3",
+                    "futureamend-note-body", "note-body-flush3_hang4", "note-body-flush0_hang4",
+                    "note-body-3em"])
+        
+
 xmlString = open("./data/olrc/archives/xhtml/1999/1999usc42.htm").read()
 #for before, after in entities:
 #    xmlString = xmlString.replace(before, after.encode('utf8'))
 
 print time.time()
-xmlSoup = BeautifulSoup(xmlString, "lxml")
-xmlSoup.findAll(text=lambda text:isinstance(text, Comment))
+#xmlSoup = BeautifulSoup(xmlString, "lxml")
+#xmlSoup.findAll(text=lambda text:isinstance(text, Comment))
 print time.time()
-parser = etree.XMLParser(remove_comments=True, recover=True)
-lxmlTree = etree.parse("./data/olrc/archives/xhtml/1999/1999usc42.htm", parser)
+parser = etree.XMLParser(remove_comments=True, recover=True, remove_pis=True, ns_clean=True)
+lxmlTree = etree.parse("./data/olrc/archives/xhtml/1995/1995usc01.htm", parser)
+
+#for child in lxmlTree.getroot():
+#    if child.tag.split("}")[1] == "emph":
+#        print type(child), child.tag, child.attrib.get('class'), child.text
+
+#for child in dom.getroot():
+#    print type(child), child.tag, child.attrib.get('class'), child.text
+
 print time.time()
