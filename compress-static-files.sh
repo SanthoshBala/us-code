2a97b23c73cdb14a964194c3045a5571e24b93f1
Santhosh Balasubramanian <santhoshbala@gmail.com>
2018-08-15 16:18:01 -0700

    update startup scripts

---
+++ compress-static-files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pwd
+cd templates
+for i in 'ls':
+	echo i
+done
--- local-deploy.sh
+++ local-deploy.sh
@@ -1,1 +1,5 @@
+
+tar -cf ./static/preamble.tar ./static/preamble.txt
+rm ./static/preamble.txt
+
 dev_appserver.py app.yaml
